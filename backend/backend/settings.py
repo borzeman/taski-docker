55da8e84e2113e4ab4317a6ee670024323a4df12
Lundovskikh Kirill <borzeman@gmail.com>
2023-09-26 13:55:21 +0500

    settings.py corrections

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / 'backend_static/static'
 
 
 # Default primary key field type
